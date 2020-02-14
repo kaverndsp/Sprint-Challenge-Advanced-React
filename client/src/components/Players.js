75c752e8094d350bcafb6a259979906b4f11ee18
kaverndsp <kaverndsp@users.noreply.github.com>
2020-02-14 10:22:42 -0600

    setting up class component and adding axios

---
+++ client/src/components/Players.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Players extends React.Component{
+    state={
+        players: []
+    }
+
+
+
+    render() {
+        return(
+                <div>
+                    
+                </div>
+        )
+    }
+
+}
+
+export default Players;
